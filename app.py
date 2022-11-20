c24cb4bf305da8cf169525e8bc8a5a28868132a0
Anubhav Lohani <anubhavlohani4@gmail.com>
2022-11-20 19:54:35 +0530

    idk dude

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
+import os
+from werkzeug.security import generate_password_hash
 
 from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
+
+curr_dir = os.path.abspath(os.path.dirname(__name__))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(curr_dir, 'database.sqlite3')
+db = SQLAlchemy()
+db.init_app(app)
+app.app_context().push()
+
+
+
+# ORM
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=False, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
+    fn_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
+
 
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        return render_template('home.html')
+        form_data = request.form.to_dict()
+        form_data['password'] = generate_password_hash(form_data['password'], 'sha256')
+
     return render_template('register.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,6 +46,7 @@ def login():
         return render_template('home.html')
 
     return render_template('login.html')
+
 
 
 if __name__ == '__main__':
