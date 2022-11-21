175221bd7dc89b3b90f4d99fec0f8cc4d9a473c3
Anubhav Lohani <anubhavlohani4@gmail.com>
2022-11-21 21:00:44 +0530

    working username validation

--- static/components/login.js
+++ static/components/login.js
@@ -4,13 +4,18 @@ const login = {
     <h3 class="mb-3 center">Login</h3>
     
     <div class="mb-3">
-      <label for="id_userEmail" class="form-label">Username</label>
-      <input type="text" name="username" class="form-control" id="id_username">
+      <label for="username" class="form-label">Username</label>
+      <input id="username" class="form-control" v-model="username" type="text" name="username">
+      <ul v-if="usernameErrors.length">
+        <li v-for="error in usernameErrors" class="error-condition">
+          {{ error }}
+        </li>
+      </ul>
     </div>
 
     <div class="mb-3">
-      <label for="id_password" class="form-label">Password</label>
-      <input type="password" name="password" class="form-control" id="id_password">
+      <label for="password" class="form-label">Password</label>
+      <input id="password" class="form-control" v-model="password" type="password" name="password">
     </div>
 
     <div class="mb-3">
@@ -20,7 +25,35 @@ const login = {
     
     <button type="submit" class="btn btn-outline-primary">Submit</button>
   </form>
-  `
+  `,
+
+  data() {
+    return {
+      username: null,
+      validUsername: false,
+      usernameErrors: [],
+      password: null,
+      validPassword: false,
+      passwordErros: [],
+    }
+  },
+  watch: {
+    username: function () {
+      this.validUsername = true
+      this.usernameErrors = []
+
+      if (this.username.trim().length == 0) {
+        this.validUsername = false
+        this.usernameErrors.push('username field cannot be empty')
+      } else {
+        var re = RegExp("[^\\w]")
+        if (this.username.length < 5 || this.username[0] == '_' || re.test(this.username)) {
+          this.validUsername = false
+          this.usernameErrors.push('username invalid')
+        }
+      }
+    }
+  }
 }
 
 export default login
